05ca16b82d5fe37e735adc8c58f5cf2811178e37
Shaik Usman <usmn_sk@hotmail.com>
2022-02-27 19:19:39 +0000

    First Webtest

---
+++ BigApp/src/Test/First.java
@@ -0,0 +1,17 @@
+package Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class First {
+	public static void main(String[] args) throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "/home/usmnskhotmail/Desktop/WebDriver/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://login.yahoo.com");
+		driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("xyzz@yahoo.com");
+	}
+}
